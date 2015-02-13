3a3e5d4700a0ce1733133ae93fe8cf121475c591
kalinzstoev <kalinzstoev@gmail.com>
2015-02-13 18:19:18 +0000

    FIrst commit

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,11 +1,5 @@
-/**
- * Created by kalinzstoev on 03/02/15.
- */
-
-    //returns all the posts from the browser database
 Template.postsList.helpers({
-    posts: function ()
-    {
-        return Posts.find();
+    posts: function() {
+        return Posts.find({}, {sort: {submitted: -1}});
     }
 });
